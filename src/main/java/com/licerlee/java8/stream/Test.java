b9c9384a8bbbabfb4636e577721fa1bc121063d1
licerlee <licerlee@gmail.com>
2019-01-13 17:58:22 +0800

    格式化

--- src/main/java/com/licerlee/java8/stream/Test.java
+++ src/main/java/com/licerlee/java8/stream/Test.java
@@ -7,26 +7,29 @@ import java.util.concurrent.TimeUnit;
 
 public class Test {
 
-	public static void main(String[] args) {
-		int max = 1000000;
-		List<String> values = new ArrayList<>(max);
-		for (int i = 0; i < max; i++) {
-			UUID uuid = UUID.randomUUID();
-			values.add(uuid.toString());
-		}
+    public static void main(String[] args) {
+        int max = 1000;
+        List<String> values = new ArrayList<>(max);
+        for (int i = 0; i < max; i++) {
+            UUID uuid = UUID.randomUUID();
+            values.add(uuid.toString());
+        }
 
-		long t0 = System.nanoTime();
+        long t0 = System.nanoTime();
 
 //		long count = values.stream().sorted().count();
-		//parallelStream
+        //parallelStream
 //		System.out.println(count);
 
-		values.parallelStream().forEach((s)->{s=s.substring(0, s.length()-1); System.out.println(s);});
-		
-		long t1 = System.nanoTime();
+        values.parallelStream().forEach((s) -> {
+            s = s.substring(0, s.length() - 1);
+            System.out.println(s);
+        });
 
-		long millis = TimeUnit.NANOSECONDS.toMillis(t1 - t0);
-		System.out.println(String.format("sequential sort took: %d ms", millis));
-	}
+        long t1 = System.nanoTime();
+
+        long millis = TimeUnit.NANOSECONDS.toMillis(t1 - t0);
+        System.out.println(String.format("sequential sort took: %d ms", millis));
+    }
 
 }
