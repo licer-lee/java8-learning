618557768e2afb87d830e2fe5bd61a321ce762d7
licerlee <licerlee@gmail.com>
2019-11-13 19:59:58 +0800

    遗留提交

--- src/main/java/com/licerlee/java8/stream/SimpleStream.java
+++ src/main/java/com/licerlee/java8/stream/SimpleStream.java
@@ -2,12 +2,14 @@ package com.licerlee.java8.stream;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class SimpleStream {
     public static void main(String[] args) {
 
         List<String> list = Arrays.asList("a", "b", "c","aaa","abc","bd","a2");
+
 
         list.stream()
                 .filter(a -> a.startsWith("a"))
@@ -19,5 +21,7 @@ public class SimpleStream {
                 .collect(Collectors.toList())
                 .forEach(System.out::println);
 
+        Optional<String> result = list.stream().filter(x -> x.startsWith("a")).reduce((a, b) -> a + "-" + b);
+        result.ifPresent(System.out::println);
     }
 }
